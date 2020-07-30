8861c6af986a8e8d1367b3c7d6027f3e46c8f08f
Ramzi Brikci-sid <brikci.sid.ramzi@gmail.com>
2020-07-30 19:09:36 +0100

    Updates

---
+++ precache-manifest.06822c71b1f2d2c34ad69c813b9d0808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8625b777d6a8385a8dc05588780213f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a2f94c5fa2df055a0be6",
+    "url": "/Portfolio/static/css/main.ad0edd9f.chunk.css"
+  },
+  {
+    "revision": "f924c43b6ac599e78afa",
+    "url": "/Portfolio/static/js/2.58998e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.58998e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f94c5fa2df055a0be6",
+    "url": "/Portfolio/static/js/main.055bed50.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "9bf6fdfa668d9622c1130689985c47e9",
+    "url": "/Portfolio/static/media/Ramzi.9bf6fdfa.jpg"
+  }
+]);
